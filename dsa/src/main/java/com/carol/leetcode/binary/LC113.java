67757bfe7f38dc13e6711799077a15228a593c30
gaoxiaolin <1327517785@qq.com>
2022-08-27 23:35:16 +0800

    leetcode113题

---
+++ dsa/src/main/java/com/carol/leetcode/binary/LC113.java
@@ -0,0 +1,54 @@
+package com.carol.leetcode.binary;
+
+import com.carol.leetcode.infra.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Carol
+ * @date 2022/8/27
+ * @since 1.0.0
+ */
+public class LC113 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        LC113 l = new LC113();
+        l.pathSum(root, 1);
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        if (null == root) {
+            return result;
+        }
+        List<Integer> tempPath = new ArrayList<>();
+        tempPath.add(root.val);
+        this.dfs(root, targetSum, root.val, tempPath);
+        return result;
+    }
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    private void dfs(TreeNode root, int targetSum, int tempSum, List<Integer> tempPath) {
+        if (null == root) {
+            return;
+        }
+        if (tempSum == targetSum && null == root.left && null == root.right) {
+            result.add(new ArrayList<>(tempPath));
+        }
+        if (null != root.left) {
+            tempPath.add(root.left.val);
+            tempSum += root.left.val;
+            dfs(root.left, targetSum, tempSum, tempPath);
+            tempPath.remove(tempPath.size() - 1);
+            tempSum -= root.left.val;
+        }
+        if (null != root.right) {
+            tempPath.add(root.right.val);
+            tempSum += root.right.val;
+            dfs(root.right, targetSum, tempSum, tempPath);
+            tempPath.remove(tempPath.size() - 1);
+            tempSum -= root.right.val;
+        }
+    }
+}
---
+++ dsa/src/main/java/com/carol/leetcode/binary/LC33.java
@@ -0,0 +1,17 @@
+package com.carol.leetcode.binary;
+
+/**
+ * @author Carol
+ * @date 2022/8/27
+ * @since 1.0.0
+ */
+public class LC33 {
+
+    /**
+     * 题目链接https://leetcode.cn/problems/search-in-rotated-sorted-array/
+     */
+
+    public int search(int[] nums, int target) {
+        return 0;
+    }
+}
